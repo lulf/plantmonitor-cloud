845bdd07edab063a78baef9fe773ad4bde3f2264
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-07-01 20:44:26 +0200

    Build native image

--- buildrelease.sh
+++ buildrelease.sh
@@ -3,7 +3,7 @@ VERSION=$1
 set -e
 
 ./mvnw clean package
-docker build -t docker.io/lulf/plantmonitor:$VERSION -f src/main/docker/Dockerfile.legacy-jar .
-#./mvnw clean package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.container-image.image=docker.io/lulf/plantmonitor:$VERSION
+#docker build -t docker.io/lulf/plantmonitor:$VERSION -f src/main/docker/Dockerfile.legacy-jar .
+./mvnw clean package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.container-image.image=docker.io/lulf/plantmonitor:$VERSION
 
 docker push docker.io/lulf/plantmonitor:$VERSION
